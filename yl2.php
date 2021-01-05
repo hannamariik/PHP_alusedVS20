420ab370fdc210ae33d19bada55135cebadacc71
hannamariik <71319264+hannamariik@users.noreply.github.com>
2021-01-05 17:10:57 +0200

    Add files via upload

---
+++ yl2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Hanna-Marii Kriisa, Ylesanne1, 23.11.2020
+ */
+$x = 8;
+$y = 2;
+$arv = 2;
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+echo "Vastused:<br> 8+2= $liitm,<br> 8-2= $lahut,<br> 8*2 $korru,<br> 8/2 $jagam,<br> $jaak<br>";
+echo "<br>Teisendamine:<br>";
+$mm = $arv * 10;
+$m = $arv / 100;
+printf("Cm : $arv <br> mm: $mm <br> meeter: $m <br>");
+
+$vastus = 2 * 4 / 2;
+echo "<br>Kolmnurga mõõdud on <br> a=2, b=4, c=6";
+
+echo "<br>Kolmnurga pindala on $vastus";
