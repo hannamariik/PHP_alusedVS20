18455dc359beb600285be703a7291a01a2675d1d
hannamariik <71319264+hannamariik@users.noreply.github.com>
2021-01-28 16:28:16 +0200

    Create yl7.php

---
+++ yl7.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Hanna-Marii Kriisa</title>
+</head>
+
+<body>
+<?php
+$break = "<br>";
+
+function tervitus(){
+    echo "Tere päiksekesekene!";
+}
+echo tervitus().$break.$break;
+
+function uudis() {
+    echo '<form action="7.php" method="get">
+        <label for="kiri">Liitu uudiskirjaga </label>
+        <input type="text" name="uudis" id="uudis" ><br>
+        <input type="submit" value="Saada">
+    </form>';
+}
+
+echo uudiskiri().$break;
+
+function kasutaja($nimi) {
+    $break = "<br>";
+    echo strtolower($nimi).$break;
+    echo strtolower($nimi)."@hkhk.edu.ee".$break;
+    echo "Kood: ".rand(1000000,9999999).$break;
+    $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.$break;
+    echo 'Kood2: '.substr(str_shuffle($permitted_chars), 6, 7).$break;
+}
+error_reporting( 0);
+
+echo kasutaja(kriisa);
+
+echo '<form action="7.php" method="get">
+        <label for="arv1">I arv </label>
+        <input type="number" name="arv1" id="arv1" ><br>
+        <label for="arv2">II arv </label>
+        <input type="arv" name="arv2" id="arv2" ><br>
+        
+        <label for="arv3">III arv </label>
+        <input type="number" name="arv3" id="arv3" ><br>
+        <input type="submit" value="arv3">
+    </form>';
+
+$nr1 = $_GET['arv1']??'';
+$nr2 = $_GET['arv2']??'';
+$nr3 = $_GET['arv3']??'';
+
+function jada($arv1,$arv2,$arv3) {
+    if($nr3 >0) {
+    for($y=$arv1;$y<=$arv2;$y += $arv3) {
+        echo $y."<br>";
+    }}
+
+}
+echo arvud($arv1,$arv2,$arv3);
+
+
+echo '<form action="7.php" method="get">
+        <label for="arva">Arv A </label>
+        <input type="arva" name="arva" id="arva" ><br>
+        <label for="arvb">Arv B </label>
+        <input type="arvb" name="arvb" id="arvb" ><br>
+        
+        <input type="submit" value="arvuta">
+    </form>';
+
+$alus = $_GET['arva']??'';
+$korgus = $_GET['arvb']??'';
+
+function pindala($alus, $korgus) {
+    echo "Ristküliku pindala: ".$arva * $arvb."<br>";
+}
+
+echo Vastus($arva,$arvb);
+
+echo '<form action="7.php" method="get">
+        <label for="isikkood">Isikukood </label>
+        <input type="isikkood" name="isikkood" id="isikkood" ><br>
+        
+        <input type="submit" value="saada">
+    </form>';
+
+$kood = $_GET['isikkood']??'';
+
+function kood($isikkood) {
+if (isset($_GET['isikkood'])) {
+if(strlen($_GET['isikkood'])) {
+    if(strlen($kood) == 11) {
+        echo "Õige isikukood "."<br>";
+        if (substr($isikkood,0,1) == 3 || substr($isikkood,0,1) == 5) {
+            echo "Mees"."<br>";
+        } else {
+            echo "Naine"."<br>";
+        }
+        echo "Sünniaeg: ".substr($isikkood, 5, 2).".".substr($isikkood, 3, 2).".".substr($isikkood, 1, 2)."<br>";
+    } else {
+        echo "Vale isikukood"."<br>";
+    }
+}}}
+
+echo isikukood($isikkood);
+
+
+function heamote() {
+    $sona = array("alus1", "alus2", "alus3");
+    $tegus = array("oeldis1", "oeldis2", "oeldis3");
+    $sihitis = array("sihitis1", "sihitis2", "sihitis3");
+
+    $nr1 = rand(0,2);
+    $nr2 = rand(0,2);
+    $nr3 = rand(0,2);
+      echo $alus[$nr1]." ".$oeldis[$nr2]." ".$sihitis[$nr3]."<br>";
+
+}
+echo heamote();
+
+
+?>
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+</html>
