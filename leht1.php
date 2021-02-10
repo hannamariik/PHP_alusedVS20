c869600edd5bcc1c9a6fff9a32c5cd0c9768d459
hannamariik <71319264+hannamariik@users.noreply.github.com>
2021-02-10 13:21:07 +0200

    Update leht1.php

--- leht1.php
+++ leht1.php
@@ -1,5 +1,11 @@
-<?php
-<h1>Leht1</h1>
-<br><br>
-<p> LoremipsumLoremipsumLoremipsumLoremipsumLoremipsumLoremipsumLoremipsum </p> 
-?>
+<html>
+  <head>
+    <title>leht1</title>
+  </head>
+  
+<body>
+  <h1>Leht1</h1>
+  <br><br>
+  <p> LoremipsumLoremipsumLoremipsumLoremipsumLoremipsumLoremipsumLoremipsum </p> 
+</body>
+</html>
